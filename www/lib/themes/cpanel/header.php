8fb51838c7263693b667235559354ac956078b66
Carlos Santana <carlos@milkzoft.com>
2012-02-04 20:39:46 -0600

    Header e idiomas

--- www/lib/themes/cpanel/header.php
+++ www/lib/themes/cpanel/header.php
@@ -27,79 +27,77 @@
 </head>
 
 <body>
-	<div class="content">
-     	<div class="row bg-white" title="header">
-		<?php 	
-			if(isset($isAdmin) and $isAdmin) { 
+	<?php
+		if($isAdmin) {
 		?>
-				<div id="top-bar">
-					<?php
-						$li[] = a("&lsaquo;&lsaquo;" . __(_("Go back")), _webBase);
-						$li[] = " | " . span("bold", __(_("Welcome"))) .": " . SESSION("ZanUser");
-						$li[] = " | " . span("bold", __(_("Online users"))) . ": $online";
-						$li[] = " | " . span("bold", __(_("Registered users"))) . ": $registered";
-						$li[] = " | " . span("bold", __(_("Last user"))) . ": " . a($lastUser["Username"], _webBase . _sh . _webLang .  "/users/editprofile/");
-						$li[] = " | " . a(__(_("Logout")) . "&rsaquo;&rsaquo;", _webBase . _sh . _webLang . "/cpanel/logout/")."";			
-							
-						print ul($li);				
-					?>
-				</div>
-		<?php 	
-			} else { 
-		?>
-				<div id="top-bar-logout">
-					<a href="<?php print _webBase; ?>" title="<?php print __(_("Go back")); ?>">&lsaquo;&lsaquo; <?php print __(_("Go back")); ?></a>
-				</div>
-		<?php	
-			} 
-		?>
-	
-		<div id="container">
-			<div id="header">
-				<div id="logo">
-					<a href="<?php print _webBase . _sh . _webLang . "/cpanel"; ?>" title="">
-						<img src="<?php print $this->themePath; ?>/images/logo.png" alt="<?php print _MuuCMS; ?>" class="no-border" />
-					</a>
-				</div>
-						
+			<div id="top-bar">
 				<?php
-					if(isset($isAdmin) and $isAdmin) {
-				?>
-						<div id="background">
-							<div id="notifications">
-								<?php 
-									if($commentsNotifications > 0) {
-										print '	<a href="'. _webBase . _sh . _webLang . "/cpanel/comments" .'" title="'. __(_("Comments")) .'">
-													<img src="'. $this->themePath .'/images/icons/comments.png" alt="'. __(_("Comments")) .'" class="no-border" /> 
-													<sup>'. $commentsNotifications .'</sup>
-												</a> ';									
-									}
-									
-									if($feedbackNotifications > 0) {
-										print '	<a href="'. _webBase . _sh . _webLang . "/cpanel/feedback" .'" title="'. __(_("Messages")) .'">
-													<img src="'. $this->themePath .'/images/icons/feedback.png" alt="'. __(_("Feedback")) .'" class="no-border" /> 
-													<sup>'. $feedbackNotifications .'</sup> 
-												</a>';
-									}
-									
-									if($galleryNotifications > 0) {
-										print '	<a href="'. _webBase . _sh . _webLang . "/cpanel/comments/gallery" .'" title="'. __(_("Gallery Comments")) .'">
-													<img src="'. $this->themePath .'/images/icons/gallery.png" alt="'. __(_("Gallery")) .'" class="no-border" /> 
-													<sup>'. $galleryNotifications .'</sup>
-												</a>';
-									}
-								?>
-							</div>
-						</div>
-						
-						<div id="route">
-							<strong><?php print __(_("You are in")); ?>:</strong> <?php print routePath(); ?>
-						</div>
-				<?php
-					} else {
-				?>
-						<br />
-				<?php
-					}
+					$li[] = anchor("&lsaquo;&lsaquo;" . __(_("Go back")), _webBase);
+					$li[] = " | " . span("bold", __(_("Welcome"))) .": " . SESSION("ZanUser");
+					$li[] = " | " . span("bold", __(_("Online users"))) . ": $online";
+					$li[] = " | " . span("bold", __(_("Registered users")) . ": $registered";
+					$li[] = " | " . span("bold", __(_("Last user"))) . ": " . anchor($lastUser["Username"], path("/users/editprofile/"));
+					$li[] = " | " . anchor(__(_("Logout")) ."&rsaquo;&rsaquo;", path("cpanel/logout/")) ."";			
+					
+					print ul($li);				
 				?>
 			</div>
+		<?php
+		} else {
+		?>
+			<div id="top-bar-logout">
+				<a href="<?php print _webBase; ?>" title="<?php print __(_("Go back")); ?>">&lsaquo;&lsaquo; <?php print __(_("Go back")); ?></a>
+			</div>
+		<?php		
+		}
+	?>
+	
+	<div id="container">
+		<div id="header">
+			<div id="logo">
+				<a href="<?php print path("cpanel"); ?>" title="">
+					<img src="<?php print $this->themePath; ?>/images/logo.png" alt="MuuCMS" class="no-border" />
+				</a>
+			</div>
+						
+			<?php
+				if($isAdmin) {
+				?>
+					<div id="background">
+						<div id="notifications">
+							<?php 
+								if($commentsNotifications > 0) {
+									print '	<a href="'. path("cpanel/comments") .'" title="'. __(_("Comments")) .'">
+												<img src="'. $this->themePath .'/images/icons/comments.png" alt="'. __(_("Comments")) .'" class="no-border" /> 
+												<sup>'. $commentsNotifications .'</sup>
+											</a> ';									
+								}
+								
+								if($feedbackNotifications > 0) {
+									print '	<a href="'. path("cpanel/feedback") .'" title="'. __(_("Messages")) .'">
+												<img src="'. $this->themePath .'/images/icons/feedback.png" alt="'. __(_("Feedback")) .'" class="no-border" /> 
+												<sup>'. $feedbackNotifications .'</sup> 
+											</a>';
+								}
+								
+								if($galleryNotifications > 0) {
+									print '	<a href="'. path("cpanel/comments/gallery") .'" title="'. __(_("Gallery Comments")) .'">
+												<img src="'. $this->themePath .'/images/icons/gallery.png" alt="'. __(_("Gallery")) .'" class="no-border" /> 
+												<sup>'. $galleryNotifications .'</sup>
+											</a>';
+								}
+							?>
+						</div>
+					</div>
+					
+					<div id="route">
+						<strong><?php print __(_("You are in")); ?>:</strong> <?php print routePath(); ?>
+					</div>
+				<?php
+				} else {
+				?>
+					<br />
+				<?php
+				}
+			?>
+		</div>
