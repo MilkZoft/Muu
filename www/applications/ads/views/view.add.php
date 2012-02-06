7d1c9a18f9198c12e222461afa58293c77af368c
Carlos Santana Roldán <carlos@milkzoft.com>
2012-02-06 15:07:56 -0600

    Update www/applications/ads/views/view.add.php

--- www/applications/ads/views/view.add.php
+++ www/applications/ads/views/view.add.php
@@ -3,32 +3,17 @@
 		die("Error: You don't have permission to access here...");
 	}
 	
-	if(isset($data)) {		
-		$ID  	   = recoverPOST("ID",        $data[0]["ID_Ad"]);
-		$title     = recoverPOST("title",     $data[0]["Title"]);
-		$URL       = recoverPOST("URL",       $data[0]["URL"]);
-		$banner    = recoverPOST("banner",    $data[0]["Banner"]);
-		$position  = recoverPOST("position",  $data[0]["Position"]);
-		$code      = recoverPOST("code",      $data[0]["Code"]);
-		$time 	   = recoverPOST("time",      $data[0]["Time"]);
-		$situation = recoverPOST("situation", $data[0]["Situation"]);
-		$principal = recoverPOST("principal", $data[0]["Principal"]);
-		$edit      = TRUE;	
-		$action	   = "edit";
-		$href	   = path(whichApplication() . "/cpanel/$action/$ID/");
-	} else {
-		$ID        = 0;
-		$title     = recoverPOST("title");
-		$URL       = "http://";
-		$position  = recoverPOST("position");
-		$code      = recoverPOST("code");
-		$time 	   = recoverPOST("time");
-		$situation = recoverPOST("situation");
-		$principal = recoverPOST("principal");
-		$edit      = FALSE;
-		$action	   = "save";
-		$href 	   = path(whichApplication() ."/cpanel/add/");;
-	}
+	$ID  	   = isset($data) ? recoverPOST("ID", $data[0]["ID_Ad"]) 				: 0;
+	$title     = isset($data) ? recoverPOST("title", $data[0]["Title"]) 			: recoverPOST("title");
+	$URL       = isset($data) ? recoverPOST("URL", $data[0]["URL"]) 				: "http://";		
+	$position  = isset($data) ? recoverPOST("position", $data[0]["Position"]) 		: recoverPOST("position");
+	$code      = isset($data) ? recoverPOST("code", $data[0]["Code"]) 				: recoverPOST("code");
+	$time 	   = isset($data) ? recoverPOST("time", $data[0]["Time"]) 				: recoverPOST("time");
+	$situation = isset($data) ? recoverPOST("situation", $data[0]["Situation"]) 	: recoverPOST("situation");
+	$principal = isset($data) ? recoverPOST("principal", $data[0]["Principal"]) 	: recoverPOST("principal");
+	$edit      = isset($data) ? TRUE 												: FALSE;	
+	$action	   = isset($data) ? "edit" 												: "save";
+	$href	   = isset($data) ? path(whichApplication() . "/cpanel/$action/$ID/") 	: path(whichApplication() ."/cpanel/add/");
 
 	print div("add-form", "class");
 		print formOpen($href, "form-add", "form-add");
