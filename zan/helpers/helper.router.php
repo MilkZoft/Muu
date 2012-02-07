2e51eac6c456dad0c1b3c7538b362ff0db342aea
Carlos Santana <carlos@milkzoft.com>
2012-02-06 19:50:26 -0600

    Segment

--- zan/helpers/helper.router.php
+++ zan/helpers/helper.router.php
@@ -414,9 +414,9 @@ function route() {
  * @param int $segment
  * @return mixed		
  */
-function segment($segment = 0) {
+function segment($segment = 0, $isLang = FALSE) {
 	$route   = route();
-	$segment = (isLang()) ? $segment + 1 : $segment;
+	$segment = ($isLang) ? $segment + 1 : $segment;
 
 	if(count($route) > 0) {		
 		if(isset($route[$segment]) and strlen($route[$segment]) > 0) {
